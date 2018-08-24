946be86ba79c4275ef9726e04512ac3ceefd4859
Rohan <rohankangutkar170594@gmail.com>
2018-08-25 02:20:00 +0530

    responsive accordion v2

--- js/script.js
+++ js/script.js
@@ -1,11 +1,10 @@
 $(document).ready(() => {
-
-	let accordion = () => {
-		$(".accordion-button").click(function(e){
-			e.preventDefault();
-			$(this).toggleClass(".active");
-			$(this).next(".content").slideToggle(500);
-		});
-	}
 	accordion();
 });
+let accordion = () => {
+	$(".accordion-button").click(function(e){
+		e.preventDefault();
+		$(this).toggleClass(".active");
+		$(this).next(".content").slideToggle(500);
+	});
+}	
